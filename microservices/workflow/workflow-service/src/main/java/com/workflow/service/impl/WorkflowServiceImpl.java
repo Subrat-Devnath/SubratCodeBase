d492565277dad2f2ec2035f822c05dbd4a3e1ccd
subrat devnath <subratnath63@gmail.com>
2025-07-08 22:13:32 +0530

    main: worflow changes

--- microservices/workflow/workflow-service/src/main/java/com/workflow/service/impl/WorkflowServiceImpl.java
+++ microservices/workflow/workflow-service/src/main/java/com/workflow/service/impl/WorkflowServiceImpl.java
@@ -2,7 +2,6 @@ package com.workflow.service.impl;
 
 import org.activiti.engine.RuntimeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.workflow.service.WorkFlowService;
@@ -10,15 +9,11 @@ import com.workflow.service.WorkFlowService;
 @Service
 public class WorkflowServiceImpl implements WorkFlowService {
 
-	@Value("${workflow.url}")
-	private String url;
-
 	@Autowired
 	private RuntimeService runtimeService;
 
 	@Override
 	public void printMessage(String processDefinationKey) {
-		System.out.println("WORKFLOW URL: " + url);
 		runtimeService.startProcessInstanceByKey(processDefinationKey);
 	}
 
